7f423bd7e735d0d69668fb1e30e4283eb9a1b835
albertbrook <3226521643@qq.com>
2020-02-27 16:29:23 +0800

    add Program.cs

---
+++ c#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace MagicCube
+{
+    public class Program : Form
+    {
+        private readonly IContainer components = null;
+
+        private Program()
+        {
+            this.Text = "Magic Cube - AlbertBrook";
+            this.ClientSize = Settings.ScreenSize;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Settings.ScreenColor;
+
+            Functions.GetFunctions(this);
+        }
+
+        public static void Main()
+        {
+            Application.Run(new Program());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
